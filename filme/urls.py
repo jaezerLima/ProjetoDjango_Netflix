b86debcd5ce8137f30ed4d9fd5060995646f1c53
Jaezer Lima <jaezer@4blue.com.br>
2022-10-31 18:52:54 -0300

    Criação da tabela Episodios

--- filme/urls.py
+++ filme/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import Homepage, Homefilmes, Detalhesfilme
 
+app_name = 'filme'
+
 urlpatterns = [
-    path('', Homepage.as_view()),
-    path('filmes/', Homefilmes.as_view()),
-    path('filmes/<int:pk>', Detalhesfilme.as_view()),
+    path('', Homepage.as_view(), name='homepage'),
+    path('filmes/', Homefilmes.as_view(), name='homefilmes'),
+    path('filmes/<int:pk>', Detalhesfilme.as_view(), name='detalhesfilme'),
 ]
