3bf27ab16b792207bdc29121d78f01583e344779
Jaezer Lima <jaezer@4blue.com.br>
2023-01-09 00:36:38 -0300

    Terminado a tela de HomeFilmes

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -2,8 +2,9 @@ from .models import Filme
 
 
 def lista_filmes_recentes(request):
-    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:10]
-    return {"lista_filmes_recentes": lista_filmes}
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:8]
+    filme_destaque = lista_filmes[0]
+    return {"lista_filmes_recentes": lista_filmes, "filme_destaque": filme_destaque}
 
 
 def lista_filmes_alta(request):
